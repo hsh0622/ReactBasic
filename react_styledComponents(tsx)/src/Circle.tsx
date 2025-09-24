375427c9ea64e4915bad1e636361a1d6e63cbc38
한승환 <coderrornotfound@gmail.com>
2025-09-24 11:17:56 +0900

    타입스크립트를 활용한 도형 꾸미기

---
+++ react_styledComponents(tsx)/src/Circle.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
+`;
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+}
+
+function Circle({ bgColor, borderColor }: CircleProps) {
+  // const [value, setValue] = useState<number | string>(0);
+  // setValue(2);
+  return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
+}
+export default Circle;
