fd05cb3b087516d7cba0a3b286d4fc44d19e5d39
한승환 <coderrornotfound@gmail.com>
2025-09-25 08:46:49 +0900

    리액트를 이용한 라우터(v6)

---
+++ react(tsx)_router/src/components/ErrorComponent.tsx
@@ -0,0 +1,5 @@
+function ErrorComponent() {
+  return <h1>Error occurred!</h1>;
+}
+
+export default ErrorComponent;
---
+++ react(tsx)_router/src/screens/users/User.tsx
@@ -0,0 +1,23 @@
+import { Outlet, useParams } from "react-router-dom";
+import { users } from "../../db";
+import { Link } from "react-router-dom";
+
+function User() {
+  const { userId } = useParams();
+  return (
+    <div>
+      <h1>
+        User with it {userId} is named: {users[Number(userId) - 1].name}
+      </h1>
+      <hr />
+      <Link to="followers">Followers</Link>
+      <Outlet
+        context={{
+          nameOfMyUser: users[Number(userId) - 1].name,
+        }}
+      />
+    </div>
+  );
+}
+
+export default User;
